c28a49bd0dd28016e8298a850bba403da6a65fb8
Greg Holmes <greg.holmes@housesimple.com>
2019-06-05 15:53:35 +0100

    first commit

---
+++ managers.js
@@ -0,0 +1,33 @@
+let managers = [
+  { id: 1, name: "Jürgen Klopp" },
+  { id: 2, name: "Mauricio Pochettino" },
+  { id: 3, name: "Ralf Rangnick" },
+  { id: 4, name: "Bruno Lage" },
+  { id: 5, name: "Maurizio Sarri" },
+  { id: 6, name: "Massimiliano Allegri" },
+  { id: 7, name: "Peter Bosz" },
+  { id: 8, name: "Paulo Fonseca" },
+  { id: 9, name: "Ernesto Valverde" },
+  { id: 10, name: "Carlo Ancelotti" },
+  { id: 11, name: "Thomas Tuchel" },
+  { id: 12, name: "Philippe Clement" },
+  { id: 13, name: "Diego Simeone" },
+  { id: 14, name: "Gian Piero Gasperini" },
+  { id: 15, name: "Christophe Galtier" },
+  { id: 16, name: "Fatih Terim" },
+  { id: 17, name: "Marcelino García Toral" },
+  { id: 18, name: "Niko Kovač" },
+  { id: 19, name: "Sergei Semak" },
+  { id: 20, name: "Pep Guardiola" },
+  { id: 21, name: "Lucien Favre" },
+  { id: 22, name: "Yuri Semin" },
+  { id: 23, name: "Marco Rose" }
+];
+
+const getManagers = () => {
+  return managers;
+};
+
+module.exports = {
+  getManagers
+};
---
+++ teams.js
@@ -0,0 +1,33 @@
+let teams = [
+  { id: 1, name: "Liverpool", country: "England", managerId: 1 },
+  { id: 2, name: "Tottenham Hotspur", country: "England", managerId: 2 },
+  { id: 3, name: "RB Leipzig", country: "Germany", managerId: 3 },
+  { id: 4, name: "Benefica", country: "Portugal", managerId: 4 },
+  { id: 5, name: "Chelsea", country: "England", managerId: 5 },
+  { id: 6, name: "Juventus", country: "Italy", managerId: 6 },
+  { id: 7, name: "Bayer Leverkusen", country: "Germany", managerId: 7 },
+  { id: 8, name: "Shakhtar Donetsk", country: "Ukraine", managerId: 8 },
+  { id: 9, name: "Barcelona", country: "Spain", managerId: 9 },
+  { id: 10, name: "Napoli", country: "Italy", managerId: 10 },
+  { id: 11, name: "Paris Saint-Germain", country: "France", managerId: 11 },
+  { id: 12, name: "Genk", country: "Belgium", managerId: 12 },
+  { id: 13, name: "Atletico Madrid", country: "Spain", managerId: 13 },
+  { id: 14, name: "Atalanta", country: "Italy", managerId: 14 },
+  { id: 15, name: "Lille", country: "France", managerId: 15 },
+  { id: 16, name: "Galatasaray", country: "Turkey", managerId: 16 },
+  { id: 17, name: "Valencia", country: "Spain", managerId: 17 },
+  { id: 18, name: "Bayern Munich", country: "Germany", managerId: 18 },
+  { id: 19, name: "Zenit Saint Petersburg", country: "Russia", managerId: 19 },
+  { id: 20, name: "Manchester City", country: "England", managerId: 20 },
+  { id: 21, name: "Borussia Dortmund", country: "Germany", managerId: 21 },
+  { id: 22, name: "Lokomotiv Moscow", country: "Russia", managerId: 22 },
+  { id: 23, name: "Red Bull Salzburg", country: "Austria", managerId: 23 }
+];
+
+const getTeams = () => {
+  return teams;
+};
+
+module.exports = {
+  getTeams
+};
